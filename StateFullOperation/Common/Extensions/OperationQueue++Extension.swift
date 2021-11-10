44cf29519ba0012b942bbb157b6660705c537873
kiarashvosough1999 <vosough.k@gmail.com>
2021-11-10 21:07:19 +0330

    addOperation is now @discardableResult and return SafeOperation

--- StateFullOperation/Common/Extensions/OperationQueue++Extension.swift
+++ StateFullOperation/Common/Extensions/OperationQueue++Extension.swift
@@ -9,19 +9,22 @@ import Foundation
 
 extension OperationQueue {
     
+    @discardableResult
     public func addOperation(identifier: OperationIdentifier = .unique(),
                              queuePriority: Operation.QueuePriority = .normal,
                              qualityOfService: QualityOfService = .default,
-                             _ operation: SFOAlias.OperationBlock,
+                             _ operationBlock: @escaping SFOAlias.OperationBlock,
                              onCompleted: SFOAlias.OnOperationCompltion?,
                              onCanceled: SFOAlias.OnOperationCanceled? = nil,
                              onFinished: SFOAlias.OnOperationFinished? = nil,
-                             onExecuting: SFOAlias.OnOperationExecuting? = nil) throws {
+                             onExecuting: SFOAlias.OnOperationExecuting? = nil) throws -> SafeOperation {
         let operation = SafeOperation(operationQueue: self, configuration: .init(identifier: identifier, queuePriority: queuePriority, qualityOfService: qualityOfService))
+        operation.operationExecutable = operationBlock
         operation.onCompleted = onCompleted
         operation.onCanceledAction = onCanceled
         operation.onFinishedAction = onFinished
         operation.onExecutingAction = onExecuting
         try operation.enqueueSelf()
+        return operation
     }
 }
